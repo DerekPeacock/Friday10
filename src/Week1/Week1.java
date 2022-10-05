9a3540c3d60b99d7d56e6d935ef3294008a037be
Derek Peacock <derek.peacock@yahoo.com>
2022-10-05 15:00:38 +0100

    Completed area

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -1,12 +1,13 @@
 package Week1;
 
 import helpers.InputReader;
+import helpers.ConsoleColours;
+
 public class Week1 
 {
-    public static final String CONSOLE_CLEAR = "\033[H\033[2J";
-    public static void main(String[] args)
+    public static void main(String[] args )
     {
-        System.out.println(CONSOLE_CLEAR);
+        System.out.println(ConsoleColours.CONSOLE_CLEAR);
 
         System.out.println("==================================");
         System.out.println("CO452 Programming Concepts 2022/23");
@@ -14,10 +15,24 @@ public class Week1
         System.out.println();
         System.out.println("  Week 1 Java Basic Input/Output ");
         System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
+        System.out.println(" by Derek Peacock");
         System.out.println();
 
-        String name = InputReader.getString("Enter your name > ");
-        System.out.println("Hello " + name);
-    } 
+     
+        calculateRectangle();
+
+    }
+    
+    public static void calculateRectangle()
+    {
+        System.out.println("Calculate Rectangle");
+        System.out.println("-------------------\n");
+
+        int length = InputReader.getInt("Enter length of rectangle in cm > ");
+        int width = InputReader.getInt("Enter width of rectangle in cm > ");        
+        
+        int area = length * width;
+
+        System.out.println("\nArea = " + area + " sq cm\n");
+    }
 }
