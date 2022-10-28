0cd59dca2960f9f17fa42693d8ee45c0a19afd08
Derek Peacock <derek.peacock@yahoo.com>
2022-10-28 10:57:36 +0100

    Added song

---
+++ src/CW1/Song.java
@@ -0,0 +1,19 @@
+package CW1;
+
+public class Song 
+{
+    private String id;
+
+    private String title;
+
+    private String artist;
+
+    private int playCount;
+
+
+    public void print()
+    {
+        System.out.println(title + " " + artist + " " + playCount);
+    }
+
+}
